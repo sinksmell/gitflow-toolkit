6501ae4ea5e6147ddf16f5f85a66d74f456e28ae
mritd <mritd1234@gmail.com>
2018-07-19 12:33:14 +0800

    style(release script): format script
    format script
    
    Signed-off-by: mritd <mritd1234@gmail.com>

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@
 
 if [ ! -n "$1" ]; then
     echo "Error:release version is blank!"
-	exit 1
+    exit 1
 fi
 
 gox -osarch="darwin/amd64 linux/386 linux/amd64" -output="dist/{{.Dir}}_{{.OS}}_{{.Arch}}"
